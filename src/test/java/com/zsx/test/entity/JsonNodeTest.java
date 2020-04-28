0372ee78d163f99bcbdf567a505bc3fdf22769c5
zsx <18273117003@163.com>
2020-04-28 10:36:33 +0800

    jsonNode List

--- src/test/java/com/zsx/test/entity/JsonNodeTest.java
+++ src/test/java/com/zsx/test/entity/JsonNodeTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
@@ -32,7 +34,7 @@ public class JsonNodeTest {
         Map<String, Object> map = Map.of("realmCode", "Butler",
                 "modelName", "Permission",
                 "dataId", "kcrfXMRp",
-                "referenceInfoList", List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk")));
+                "referenceInfoList", List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk", "remarks")));
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = mapper.valueToTree(map);
         System.out.println(jsonNode);
@@ -61,30 +63,34 @@ public class JsonNodeTest {
         Map<String, Object> map = Map.of("realmCode", "Butler",
                 "modelName", "Permission",
                 "dataId", "kcrfXMRp",
-                "referenceInfoList", List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk")));
+                "referenceInfoList", List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk", "remarks")));
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = mapper.valueToTree(map);
         System.out.println(jsonNode);
-        Map<String, Object> result = mapper.convertValue(jsonNode, new TypeReference<>(){});
-        result.remove("referenceInfoList");
-        System.out.println(result);
-        JsonNode jsonNode1 = mapper.valueToTree(result);
-        System.out.println(jsonNode1);
-        Iterator<String> fieldNames = jsonNode1.fieldNames();
-        while(fieldNames.hasNext()) {
-            System.out.println(fieldNames.next());
+        if (jsonNode instanceof ObjectNode) {
+            Map<String, Object> result = mapper.convertValue(jsonNode, new TypeReference<>(){});
+            result.remove("referenceInfoList");
+            System.out.println(result);
+            JsonNode jsonNode1 = mapper.valueToTree(result);
+            System.out.println(jsonNode1);
+            Iterator<String> fieldNames = jsonNode1.fieldNames();
+            while(fieldNames.hasNext()) {
+                System.out.println(fieldNames.next());
+            }
         }
 
     }
 
     @Test
     void testListRemoveNode() {
-        List<ReferenceInfo> infos = List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk"));
+        List<ReferenceInfo> infos = List.of(ReferenceInfo.of("PermissionManager", "RoleGroup", "uEhJIADk", "remarks"));
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = mapper.valueToTree(infos);
         System.out.println(jsonNode);
-        List<ReferenceInfo> infos1 = mapper.convertValue(jsonNode, new TypeReference<>(){});
-        System.out.println(infos1);
+        if (jsonNode instanceof ArrayNode) {
+            List<ReferenceInfo> infos1 = mapper.convertValue(jsonNode, new TypeReference<>(){});
+            System.out.println(infos1);
+        }
     }
 
 
--- src/test/java/com/zsx/test/entity/ReferenceInfo.java
+++ src/test/java/com/zsx/test/entity/ReferenceInfo.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.springframework.data.annotation.Transient;
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
@@ -14,4 +17,7 @@ public class ReferenceInfo {
     public String referringModelName;
 
     public String referringDataKey;
+
+    @Transient
+    public String remarks;
 }
